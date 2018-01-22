72a2ea1673968f4c0747e7f76edea24f116d0034
Ferdinand <ferdinand.chandra@happyfresh.com>
2018-01-22 22:01:32 +0700

    finishing

--- index.js
+++ index.js
@@ -5,29 +5,15 @@ const fs = require('fs');
 const prefix = 'https://api.mlab.com/api/1';
 const API_KEY = process.env.API_KEY;
 axios.defaults.headers.post['Content-Type'] = 'application/json';
-const set = new Set();
-
-function createNotebook(name) {
-  const notebook = new Object();
-  notebook.name = name;
-  return notebook;
-}
 
 function createListOfNotebook() {
   const enterKomputerApi = 'https://enterkomputer.com/api/product/notebook.json';
+  const arrayNotebook = new Array();
   
   axios.get(enterKomputerApi)
   .then(function(response) {
     const data = response.data;
-    let outputString = '';
-    let outputString2 = '';
-
-    let notebookName = '';
-    let notebookDetails = '';
-    let notebookBrand = '';
-    let notebookSubcategoryDescription = '';
-    let notebookPrice = '';
-    let notebookQuantity = '';
+    let notebookObj = new Object();
     
     data.map(notebook => {
       if (notebook.subcategory_description != 'Notebook LED Panel' &&
@@ -41,39 +27,19 @@ function createListOfNotebook() {
           notebook.subcategory_description != 'Apple Cable / Connector' &&
           notebook.subcategory_description != '') {
             
-            notebookName = notebook.name;
-            notebookName = cleanName(notebookName);
+            notebookObj.name = cleanName(notebook.name);
             
-            // TO BE PARSE LATER
-            notebookDetails = notebookDetailsParse(notebook.details);
+            notebookObj.details = notebookDetailsParse(notebook.details);
 
-            notebookBrand = notebook.brand_description
+            notebookObj.brand = notebook.brand_description
 
-            // TO BE ANALYZED
-            notebookSubcategoryDescription = notebook.subcategory_description;
-
-            notebookPrice = notebook.price;
-            notebookQuantity = notebook.quantity;
+            notebookObj.price = notebook.price;
             
-            outputString  += `${notebookDetails}\n`;
-            outputString2 += `${notebookName}\n`;
+            arrayNotebook.push(notebookObj);
       }
     });
-    fs.writeFile('out.txt', outputString, (err) => {  
-      // throws an error, you could also catch it here
-      if (err) throw err;
-  
-      // success case, the file was saved
-      console.log('Done!');
-    });
-    fs.writeFile('out_name.txt', outputString2, (err) => {  
-      // throws an error, you could also catch it here
-      if (err) throw err;
-  
-      // success case, the file was saved
-      console.log('Done!');
-    });
-    console.log(set.size);
+
+    insertToDB(arrayNotebook);
   });
 }
 
@@ -92,49 +58,87 @@ function notebookDetailsParse(details) {
   const detail = new Object();
 
   detail.details = details;
-  detail.processor = parsingProcessor(notebookDetails[0]);
 
-  
-
-  if (notebookDetails[1]) {
-    detail.ram = notebookDetails[1].trim();
+  // String rusak
+  if (notebookDetails[0].match(/\//g) && !notebookDetails[0].match(/\/13/g)){
+    const arrData = notebookDetails[0].split('/')
+    detail.processor = parsingProcessor(arrData[0]);
+    detail.ram = parsingRam(arrData[1]);
+    detail.storage = parsingStorage(arrData[2]);
   }
-
-  return detail.processor;
+  // String bagus
+  else {
+    detail.processor = parsingProcessor(notebookDetails[0]);
+    detail.ram = parsingRam(notebookDetails[1]);
+    detail.storage = parsingStorage(notebookDetails[2]);
+  }
+  
+  return detail;
 }
 
 function parsingProcessor(data) {
   let processor = '';
   // PARSING PROCESSOR
   if (data) {
-    const splitProcessorString = data.split('/')
-    // String processor yang salah
-    if (splitProcessorString[1]) {
-      processor = splitProcessorString[0].trim();
-    } 
-    // String processor yang benar
-    else {
-      processor = data.trim();
-    }
-
+    processor = data.trim();
+    
     // Parsing buat intel
-    if (data.match(/intel/gi)) {
-      if (data.match(/i3/gi)) {
+    if (processor.match(/intel/gi)) {
+      if (processor.match(/i3/gi)) {
         processor = 'Intel i3';
-      } else if (data.match(/i5/gi)){
+      } else if (processor.match(/i5/gi)){
         processor = 'Intel i5';
-      } else if (data.match(/i7/gi)){
+      } else if (processor.match(/i7/gi)){
         processor = 'Intel i7';
       }
     }
 
     // Parsing buat amd
-    if (data.match(/amd/gi)) {
+    if (processor.match(/amd/gi)) {
       processor = 'AMD';
     }
   }
 
   return processor;
+}
+
+function parsingRam(data) {
+  let ram = '';
+  if (data) {
+    ram = data.trim();
+
+    if (ram.match(/2GB/gi)) {
+      ram = '2';
+    } else if (ram.match(/4GB/gi)) {
+      ram = '4';
+    } else if (ram.match(/8GB/gi)) {
+      ram = '8';
+    } else if (ram.match(/16GB/gi)) {
+      ram = '16';
+    }
+  }
+
+  return ram;
+}
+
+function parsingStorage(data) {
+  let storage = '';
+
+  if (data) {
+    storage = data.trim();
+
+    if (storage.match(/256GB/gi)) {
+      storage = '256';
+    } else if (storage.match(/500GB/gi)) {
+      storage = '500';
+    } else if (storage.match(/512GB/gi)) {
+      storage = '512';
+    } else if (storage.match(/1TB/gi)) {
+      storage = '1000';
+    }
+  }
+
+  return storage;
 }
 
 function insertToDB(data) {
@@ -150,7 +154,6 @@ function insertToDB(data) {
 }
 
 function main() {
-  // insertToDB([createNotebook("Apple Macbook Pro"), createNotebook("Apple Macbook Pro"), createNotebook("Apple Macbook Pro")]);
   createListOfNotebook();
 }
 
